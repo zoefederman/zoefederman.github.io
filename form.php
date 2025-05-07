f229339be857a124afb5b6d878740fcc3f5c9afb
zoefederman <zoe.federman@gmail.com>
2025-05-07 08:53:21 -0400

    Update form.php

--- form.php
+++ form.php
@@ -2,8 +2,8 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+Welcome <?php echo $_POST["first name"]; ?><br>
+Your email address is: <?php echo $_POST["last name"]; ?>
 
 </body>
 </html>
